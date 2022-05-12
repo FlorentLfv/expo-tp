8068972748c12409f7080605637e94db737e64a9
Florent <florentlfv@gmail.com>
2022-05-12 10:23:01 +0200

    tp

--- App.js
+++ App.js
@@ -1,20 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native'
+
+const data = require('./tp.json');
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <ScrollView style={{ paddingTop: 20 }}>
+      <View style={styles.container}>
+        {data.map((elt) => (
+          <View style={styles.eachContainer}>
+            <View style={styles.imageContainer}>
+              <Image source={{ uri: elt.img }} style={styles.image} />
+              <View style={styles.nameContainer}>
+                <Text style={styles.title}> {elt.title} </Text>
+                <View style={styles.descContainer}>
+                  <Text style={styles.description}>{elt.desc}</Text>
+                </View>
+              </View>
+            </View>
+          </View>
+        ))
+        }
+      </View >
+    </ScrollView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1
   },
-});
+  eachContainer: {
+    padding: 25,
+  },
+  imageContainer: {
+    flex: 1,
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  nameContainer: {
+    flex: 1,
+    paddingLeft: 20,
+    flexDirection: 'column',
+  },
+  descContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 150,
+    height: 150
+  },
+  title: {
+    textAlign: 'center'
+  },
+  description: {
+    textAlign: 'justify'
+  }
+})
